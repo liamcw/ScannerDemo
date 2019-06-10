1776d6a7767d077b8eb32f2785d28467a4d73955
Liam Chan-Wicht <liamchanwicht@hotmail.com>
2019-06-10 15:10:36 +1000

    commented

--- src/FriendlyGreeter.java
+++ src/FriendlyGreeter.java
@@ -4,31 +4,30 @@ public class FriendlyGreeter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner1=new Scanner(System.in);
-		
+		Scanner scanner1 = new Scanner(System.in);
+
 		System.out.print("Enter your name: ");
-		String name; 
-		name=scanner1.next();
-		
+		String name;
+		name = scanner1.next();
+
 		System.out.print("Enter your age (years): ");
 		int age;
-		age=scanner1.nextInt(); //returns an int
-		
+		age = scanner1.nextInt(); // returns an int
+
 		System.out.print("Enter your height (m): ");
 		double height;
-		height=scanner1.nextDouble();//returns a double
-		
+		height = scanner1.nextDouble();// returns a double
+
 		System.out.print("Enter your weight (Kg): ");
 		double weight = scanner1.nextDouble();
-		
+
 		double bmi = weight / (height * height);
-		
-		scanner1.close();
-		
-		System.out.println("Hello " + name +"!");
+
+		scanner1.close();// NEEDS TO BE CLOSED
+
+		System.out.println("Hello " + name + "!");
 		System.out.print("You are " + age + " and your body mass index is " + bmi + ".");
 
-		
 	}
 
 }
